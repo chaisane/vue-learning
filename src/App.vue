8a573d90ebf2a930c16bbaddf64e0ff0c959f356
chaisane <praew.somsiri@gmail.com>
2025-08-14 19:28:49 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,59 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const msg = "HelloWorld";
+const rawHtml =
+  'Using text interpolation: <span style="color: red">This should be red.</span>';
+const dynamicId = "myID";
+const id = "myID";
+const isButtonDisabled = true;
+const disabled = true;
+const objectOfAttrs = {
+  id: "container",
+  class: "wrapper",
+  style: "background-color:green",
+};
+const number = 0;
+const message = "Using JavaScript Expressions";
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <!--Text Interpolation-->
+  <span>Message: {{ msg }}</span>
+  <hr />
+  <!--Raw HTML-->
+  <p>Using text interpolation: {{ rawHtml }}</p>
+  <p>Using v-html directive: <span v-html="rawHtml"></span></p>
+  <hr />
+  <!--Attribute Bindings ใช้ได้กับ attribute ใด ๆ เช่น :src, :href, :class, :style-->
+  <div v-bind:id="dynamicId">ID Elememt</div>
+  <div :id="dynamicId">ID Elememt Shorthand</div>
+  <div :id>ID Elememt Same-name Shorthand</div>
+  <hr />
+  <!--Boolean Attributes-->
+  <button :disabled="isButtonDisabled">Button</button>
+  <br />
+  <button :disabled>Button Same-name Shorthand</button>
+  <hr />
+  <!-- Dynamically Binding Multiple Attributes -->
+  <div v-bind="objectOfAttrs">Dynamically Binding Multiple Attributes</div>
+  <hr />
+  <!--Using JavaScript Expressions-->
+  <p>{{ number + 1 }}</p>
+
+  <p>{{ isButtonDisabled ? "ButtonDisabled: YES" : "ButtonDisabled: NO" }}</p>
+
+  <p>{{ message.split("").reverse().join("") }}</p>
+
+  <div :id="`list-${id}`">
+    {{ `Using "${id}" JavaScript Expression` }}
+  </div>
+
+  <!-- ❌ Statement (คำสั่งประกาศตัวแปร) 
+  {{ var a = 1 }}
+  -->
+
+  <!-- ❌ Flow control (if, for, while) 
+   {{ if (ok) { return message } }}
+  -->
 </template>
 
 <style scoped></style>
