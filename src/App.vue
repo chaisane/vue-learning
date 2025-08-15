7abf841a9999551c9b5f823fe6387e26ea9b93de
chaisane <praew.somsiri@gmail.com>
2025-08-15 14:45:10 +0700

    update app

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,10 @@ const objectOfAttrs = {
 };
 const number = 0;
 const message = 'Using JavaScript Expressions';
+const seen: boolean = true;
+const names = ['A', 'B', 'C'];
+const url: string = 'https://vuejs.org/';
+const eventName = 'click';
 
 function toTitleDate(d: Date) {
   return d.toDateString();
@@ -23,35 +27,40 @@ function formatDate(d: Date) {
 }
 
 const date = new Date();
+
+function onAlert(ok: string) {
+  alert(ok);
+}
+
 </script>
 
 <template>
-  <!-- #Text Interpolation-->
+  <!-- Text Interpolation-->
   <span>Message: {{ msg }}</span>
   <hr />
 
-  <!--#Raw HTML-->
+  <!--Raw HTML-->
   <p>Using text interpolation: {{ rawHtml }}</p>
   <p>Using v-html directive: <span v-html="rawHtml"></span></p>
   <hr />
 
-  <!--#Attribute Bindings ใช้ได้กับ attribute ใด ๆ เช่น :src, :href, :class, :style-->
+  <!--Attribute Bindings ใช้ได้กับ attribute ใด ๆ เช่น :src, :href, :class, :style-->
   <div v-bind:id="dynamicId">ID Elememt</div>
   <div :id="dynamicId">ID Elememt Shorthand</div>
   <div :id>ID Elememt Same-name Shorthand</div>
   <hr />
 
-  <!--#Boolean Attributes-->
+  <!--Boolean Attributes-->
   <button :disabled="isButtonDisabled">Button</button>
   <br />
   <button :disabled>Button Same-name Shorthand</button>
   <hr />
 
-  <!--#Dynamically Binding Multiple Attributes -->
+  <!--Dynamically Binding Multiple Attributes -->
   <div v-bind="objectOfAttrs">Dynamically Binding Multiple Attributes</div>
   <hr />
 
-  <!--#Using JavaScript Expressions-->
+  <!--Using JavaScript Expressions-->
   <p>{{ number + 1 }}</p>
 
   <p>{{ isButtonDisabled ? "ButtonDisabled: YES" : "ButtonDisabled: NO" }}</p>
@@ -61,25 +70,35 @@ const date = new Date();
   <div :id="`list-${id}`">
     {{ `Using "${id}" JavaScript Expression` }}
   </div>
-
-  <!-- ❌ Statement (คำสั่งประกาศตัวแปร) 
-  {{ var a = 1 }}
-  -->
-
-  <!-- ❌ Flow control (if, for, while) 
-   {{ if (ok) { return message } }}
-  -->
   <hr />
 
-  <!--#Calling Functions-->
+  <!--Calling Functions-->
   <time :title="toTitleDate(date)" :datetime="date.toISOString()">
     {{ formatDate(date) }}
   </time>
   <hr />
-  <!-- #Restricted Globals Access -->
+
+  <!-- Restricted Globals Access -->
   <p>globalProperties: {{ $appName }}</p>
   <hr />
-  <!--#Directives -->
+
+  <!--Directives -->
+  <p v-if="seen">Now you see me</p>
+  <ul v-for="name of names">
+    <li>{{ name }}</li>
+  </ul>
+  <hr>
+
+  <!--Arguments-->
+  <a :href="url">Click link</a>
+  <div style="margin-top: 10px">
+        <!-- v-on:click -->
+        <button @click="onAlert('OK On Click!!!')">On Click</button>
+  </div>
+  <button @[eventName]="onAlert('OK On eventName!!!')" style="margin-top: 10px">
+    On eventName
+  </button>
+
 </template>
 
 <style scoped></style>
