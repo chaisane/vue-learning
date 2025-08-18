13907a77abb07181f82d05523d57cd5a41a62e8d
chaisane <praew.somsiri@gmail.com>
2025-08-18 14:35:58 +0700

    reactive

--- src/components/ReactivityFundamentals.vue
+++ src/components/ReactivityFundamentals.vue
@@ -1,10 +1,11 @@
 <script setup>
-import { ref } from 'vue'
+import { reactive, ref } from 'vue'
+
 
 const countA = 0;
 console.log('countA: ', countA);
 
-// ใช้ ref เหมาะกับค่าพื้นฐาน (number, string, boolean) หรือ object/array เล็ก ๆ
+// ใช้ ref() เหมาะกับค่าพื้นฐาน (number, string, boolean) หรือ object/array เล็ก ๆ
 const countB = ref(0);
 console.log('countB: ', countB);
 function increment() {
@@ -22,18 +23,57 @@ function manageObject() {
     obj.value.arr.push('C');
 }
 
-const objArr = ref([
-    {
-        num: { count: 0 },
-        arr: ['A', 'B']
-    },
-    {
-        num: { count: 10 },
-        arr: ['X', 'Y']
-    }
-]);
 
-console.log('objArr: ', objArr.value[1].arr[0]);
+// ใช้ reactive() เมื่อเก็บ object/array ซับซ้อน หรือ Map, Set
+const book = reactive({
+    title: 'Clean Code',
+    author: {
+        name: 'Robert C. Martin',
+        age: 70
+    },
+    publishedYear: 2008,
+    categories: ['Programming', 'Software Engineering'],
+    stock: 3,
+    reviews: [
+        { user: 'Alice', rating: 5, comment: 'Must-read for developers!' },
+        { user: 'Bob', rating: 4, comment: 'Great but a bit long.' }
+    ]
+});
+
+function addReview(user, rating, comment) {
+    book.reviews.push({ user, rating, comment })
+}
+
+function restock(amount) {
+    book.stock += amount
+}
+
+function sellOne() {
+    if (book.stock > 0) book.stock--
+}
+
+// function craeteBook() {
+//     return {
+//         title: 'Clean Code',
+//         author: {
+//             name: 'Robert C. Martin',
+//             age: 70
+//         },
+//         publishedYear: 2008,
+//         categories: ['Programming', 'Software Engineering'],
+//         stock: 3,
+//         reviews: [
+//             { user: 'Alice', rating: 5, comment: 'Must-read for developers!' },
+//             { user: 'Bob', rating: 4, comment: 'Great but a bit long.' }
+//         ]
+//     };
+// }
+
+// const book = reactive(craeteBook());
+
+// function reset() {
+//     Object.assign(book, craeteBook());
+// }
 
 </script>
 
@@ -42,11 +82,38 @@ console.log('objArr: ', objArr.value[1].arr[0]);
     <div class="col-12 card h-100 card-body">
         <button @click="countA++"> increment A </button> = {{ countA }}
         <button @click="countB++"> increment B </button> = {{ countB }}
-        <button @click="increment()"> increment() B </button> = {{ countB }}
+        <button @click="increment"> increment() B </button> = {{ countB }}
     </div>
     <!-- Deep Reactivity -->
     <div class="col-12 card h-100 card-body">
-        <button @click="manageObject()">Manage Object</button>
+        <button @click="manageObject">Manage Object</button>
         <pre>{{ JSON.stringify(obj, null, 2) }}</pre>
+    </div>
+    <!-- reactive() -->
+    <div class="col-12 card h-100 card-body">
+        <div class="p-3">
+            <h2>{{ book.title }}</h2>
+            <p><b>Author:</b> {{ book.author.name }} ({{ book.author.age }})</p>
+            <p><b>Year:</b> {{ book.publishedYear }}</p>
+            <p><b>Categories:</b> {{ book.categories.join(', ') }}</p>
+            <p><b>Stock:</b> {{ book.stock }}</p>
+
+            <button @click="sellOne" class="btn btn-danger btn-sm">Sell One</button>
+            <button @click="restock(5)" class="btn btn-success btn-sm ms-2">Restock +5</button>
+
+            <h4 class="mt-3">Reviews</h4>
+            <ul>
+                <li v-for="(r, i) in book.reviews" :key="i">
+                    ⭐ {{ r.rating }}/5 - {{ r.comment }} <i>by {{ r.user }}</i>
+                </li>
+            </ul>
+
+            <button @click="addReview('Charlie', 5, 'Excellent!')" class="btn btn-primary btn-sm mt-2">
+                Add Review (Charlie)
+            </button>
+            <!-- <button @click="reset" class="btn btn-basic btn-sm mt-2">
+                Reset
+            </button> -->
+        </div>
     </div>
 </template>
