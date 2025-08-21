92e69f14dac18294aae301ca34e8b59ef1ebcb3b
chaisane <praew.somsiri@gmail.com>
2025-08-21 10:52:17 +0700

    mod style

--- src/components/EventHandling.vue
+++ src/components/EventHandling.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 
 function onAlert(msg: string) {
     alert(msg)
@@ -7,6 +8,9 @@ function onAlert(msg: string) {
 function onSubmit() {
     onAlert('Form submitted')
 }
+
+const btnMsg = ref('One Click')
+
 </script>
 
 <template>
@@ -14,39 +18,51 @@ function onSubmit() {
     <!-- Event Modifiers: ทำให้เขียน event handling ง่ายขึ้น -->
 
     <!-- @click.stop กัน event ไม่ให้ส่งขึ้น parent -->
-    <div @click="onAlert('Parent clicked')">
-        Parent Div
-        <button @click="onAlert('Button clicked')">Click Me</button>
-        <button @click.stop="onAlert('Button clicked Only')">Click Me .stop</button>
+    <div class="col-12 card h-100 card-body">
+        <div @click="onAlert('Parent clicked')" class="p-3 mb-2 bg-secondary text-white">
+            <h3>Parent Div</h3>
+            <button @click="onAlert('Button clicked')" class="btn btn-warning">Click Me</button>&nbsp;
+            <button @click.stop="onAlert('Button clicked Only')" class="btn btn-success">Click Me .stop</button>
+        </div>
     </div>
 
-    <!-- ชั้นที่ 1 .capture จากนอกสุด ไป ในสุด -->
-    <div @click="onAlert('Layer #1')" class="layer-grand">
-        Layer #1
+    <div class="col-12 card h-100 card-body">
+        <!-- ชั้นที่ 1 .capture จากนอกสุด ไป ในสุด -->
+        <div @click="onAlert('Layer #1')" class="layer-grand rounded">
+            Layer #1
 
-        <!-- ชั้นที่ 2 .self ทำงานเฉพาะคลิกตรงพื้นที่ตัวเอง -->
-        <div @click="onAlert('Layer #2')" class="layer-parent">
-            Layer #2
+            <!-- ชั้นที่ 2 .self ทำงานเฉพาะคลิกตรงพื้นที่ตัวเอง -->
+            <div @click="onAlert('Layer #2')" class="layer-parent rounded">
+                Layer #2
 
-            <!-- ชั้นที่ 3 .stop กัน event ไม่ให้ส่งขึ้นลำดับถัดไป -->
-            <div @click="onAlert('Layer #3')" class="layer-child">
-                Layer #3
+                <!-- ชั้นที่ 3 .stop กัน event ไม่ให้ส่งขึ้นลำดับถัดไป -->
+                <div @click="onAlert('Layer #3')" class="layer-child rounded">
+                    Layer #3
+                </div>
             </div>
         </div>
     </div>
 
-    <button @click.once="onAlert('Pay Now')">One Click</button>
+    <div class="col-4 card h-100 card-body">
+        <button @click.once="onAlert('Pay Now'), btnMsg = 'Lock Click'" class="btn btn-info">{{ btnMsg }}</button>
+    </div>
 
     <!-- @submit.prevent กัน reload หน้า -->
-    <form @submit="onSubmit">
-        <input :value="'Foo Bar'" />
-        <button type="submit">Submit</button>
-    </form>
-    <form @submit.prevent="onSubmit">
-        <input :value="'Foo Bar.prevent'" />
-        <button type="submit">Submit</button>
-    </form>
-
+    <div class="col-4 card h-100 card-body">
+        <h4>Form</h4>
+        <form @submit="onSubmit">
+            <div class="input-group mb-3">
+                <input type="text" class="form-control" :value="'reload'">
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </div>
+        </form>
+        <form @submit.prevent="onSubmit">
+            <div class="input-group mb-3">
+                <input type="text" class="form-control" :value="'not reload '">
+                <button type="submit" class="btn btn-primary">Submit.prevent</button>
+            </div>
+        </form>
+    </div>
 
 </template>
 
@@ -54,15 +70,18 @@ function onSubmit() {
 .layer-grand {
     padding: 20px;
     background: #aaa;
+    cursor: pointer;
 }
 
 .layer-parent {
     padding: 20px;
     background: #ccc;
+    cursor: pointer;
 }
 
 .layer-child {
     padding: 20px;
     background: #eee;
+    cursor: pointer;
 }
 </style>
